e0a84fc1cf94b926fe62b61b0157186f8cf9c23e
Charles Duprey <barbatos@urbanterror.info>
2013-12-22 01:40:44 +0100

    Put the structs in common.h

--- common.h
+++ common.h
@@ -14,9 +14,24 @@
 #include <QDialogButtonBox>
 #include <QPushButton>
 #include <QLabel>
+#include <QComboBox>
+#include <QInputDialog>
 
 #define URT_GAME_NAME       "Urban Terror"
 #define URT_GAME_SUBDIR     "q3ut4"
 #define URT_API_LINK        "http://barbatos.fr/updater/api.php"
+
+typedef struct {
+    QString fileName;
+    QString fileMd5;
+    QString filePath;
+    QString fileSize;
+} fileInfo_s;
+
+typedef struct {
+    QString serverName;
+    QString serverURL;
+    QString serverLocation;
+} serverInfo_s;
 
 #endif // COMMON_H

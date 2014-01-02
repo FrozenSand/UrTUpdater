2c27bb30cfe25399a6f1bcdfb5acc8ba30965cfc
Charles Duprey <barbatos@urbanterror.info>
2014-01-02 14:04:02 +0100

    Licence stuff

--- settings.h
+++ settings.h
@@ -1,3 +1,26 @@
+/**
+  * Urban Terror Updater
+  *
+  * This software is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU Lesser General Public
+  * License as published by the Free Software Foundation; either
+  * version 2.1 of the License, or (at your option) any later version.
+  *
+  * This software is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  * Lesser General Public License for more details.
+  *
+  * You should have received a copy of the GNU Lesser General Public
+  * License along with this software; if not, write to the Free Software
+  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+  *
+  * @version    4.0.1
+  * @author     Charles 'Barbatos' Duprey
+  * @email      barbatos@urbanterror.info
+  * @copyright  2013/2014 Frozen Sand / 0870760 B.C. Ltd
+  */
+
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
